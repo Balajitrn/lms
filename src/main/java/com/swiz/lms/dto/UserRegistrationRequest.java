e372f21494229907746f457ac8f20dcdf686b046
Balajitrn <balamaddy03@gmail.com>
2023-08-08 15:25:19 +0530

    Initial commit

---
+++ src/main/java/com/swiz/lms/dto/UserRegistrationRequest.java
@@ -0,0 +1,27 @@
+package com.swiz.lms.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegistrationRequest {
+
+    private String username;
+    private String email;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "UserRegistrationRequest{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+
+
+    // You may include other fields such as phone number, address, etc.
+
+}
